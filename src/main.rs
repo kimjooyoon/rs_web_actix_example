2c5c0a146fd58895f38b7ddffec95ea163879c88
kimjooyoon <asketeddy@gmail.com>
2023-03-18 17:22:00 +0900

    test(user): add integration test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{
     web,
     body::BoxBody, http::header::ContentType, get, App, HttpResponse, Responder, HttpServer, HttpRequest};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize,Deserialize,PartialEq,Debug)]
 struct User {
     name: String,
     age: u64,
@@ -23,6 +23,24 @@ async fn hello(path : web::Path<u64>) -> impl Responder {
     User { name: String::from("teddy"), age: path.into_inner() }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http::header::ContentType, test, App};
+
+    #[actix_web::test]
+    async fn test_hello_ok() {
+        let app = test::init_service(
+            App::new().service(hello)
+        ).await;
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::json())
+            .uri("/321")
+            .to_request();
+        let resp:User = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(resp, User{ name: "teddy".to_string(), age: 321 })
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
